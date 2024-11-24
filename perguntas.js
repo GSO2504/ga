d30b6b87232b3045c7f39091cc2d99566ffd5dbd
GSO2504 <00001088887600SP@al.educacao.sp.gov.br>
2024-11-24 17:11:05 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Geral',
+    ' Qual faculdade deve ser feita pra se tornar médico?  ',
+    'Medicina'
+)
+
+criaCartao(
+    'Geral',
+    'Qual é o vestibular que os estudantes prestam quando acabam o ensino médio ?',
+    'ENEM'
+)
+
+criaCartao(
+    'Geral',
+    'Em qual país fica o estado de São Paulo ?',
+    'Brasil'
+)
+
+criaCartao(
+    'Geral',
+    'Qual a maior processadora de cana de açúcar do mundo?',
+    'Usina São Martinho'
+)
